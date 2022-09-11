b555ad664aad7934cd14fdb22e5114bbd09e6a4d
annartemyeva <artemeva.a.o@gmail.com>
2022-09-12 00:29:11 +0300

    // 1.1.3 pattern dao (jdbc)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.io.IOException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws IOException {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Mike", "Wheeler", (byte) 20);
+        userService.saveUser("Dustin", "Henderson", (byte) 21);
+        userService.saveUser("Lucas", "Sinclair", (byte) 19);
+        userService.saveUser("Will", "Byers", (byte) 18);
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
